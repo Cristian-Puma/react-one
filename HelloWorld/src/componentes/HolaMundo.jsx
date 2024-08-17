c1858aa428e3e844fb20e34bcf907d4de12e9a04
Cristian-Puma <pumitaaldair10@gmail.com>
2024-08-17 00:08:54 +0000

    Hemos creado dos componentes desde 0

---
+++ HelloWorld/src/componentes/HolaMundo.jsx
@@ -0,0 +1,8 @@
+
+function HolaMundo(){
+
+    return <h1>Hola mundo... Cristian Puma</h1>
+
+}
+
+export default HolaMundo;
---
+++ HelloWorld/src/componentes/Mensaje.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+function Mensaje(){
+
+    return <div>
+        <h1>Programando con React</h1>
+        <h2>Cristian Puma</h2>
+    </div>
+}
+
+export default Mensaje;
